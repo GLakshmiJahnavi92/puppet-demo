3c486b64051acfefa6934132c598338c7e172b25
jahnavi05 <ljahnavi92@gmail.com>
2025-10-29 11:00:36 +0530

    Updated Puppet manifest with new configuration

--- manifest.pp
+++ manifest.pp
@@ -1,18 +1,23 @@
-# manifest.pp
+# manifest.pp - Puppet manifest for Bolt on Windows
 
+# Include chocolatey class (module must be installed in modulepath)
 include chocolatey
 
+# Ensure directory exists
 file { 'C:/puppet-demo/mydir':
   ensure => directory,
 }
 
+# Create hello.txt with content
 file { 'C:/puppet-demo/hello.txt':
   ensure  => file,
   content => "Hello Puppet!\n",
   require => File['C:/puppet-demo/mydir'],
 }
 
+# Install Apache HTTP Server via Chocolatey
 package { 'apache-httpd':
   ensure   => installed,
   provider => chocolatey,
+  require  => Class['chocolatey'],
 }
