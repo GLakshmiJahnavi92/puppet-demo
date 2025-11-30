afd89edc237a96fcae44ef2cbeeebbd3b9ec8c31
jahnavi05 <ljahnavi92@gmail.com>
2025-11-30 20:24:19 +0530

    Puppet manifest and hello.txt added

--- manifest.pp
+++ manifest.pp
@@ -11,7 +11,7 @@ file { 'C:/puppet-demo/mydir':
 # Create hello.txt with content
 file { 'C:/puppet-demo/hello.txt':
   ensure  => file,
-  content => "Hello Puppet from bolt!\n",
+  content => "Hello Puppet!\n",
   require => File['C:/puppet-demo/mydir'],
 }
 
